8de6b5e8f49efa64a1f4f6cc8ae18a43190a4756
Efimov, Aleksandr2 <a2.efimov@a1qa.com>
2018-08-01 00:37:57 +0300

    initial project version

---
+++ OnlineStore/PageObjects/HomePage.cs
@@ -0,0 +1,18 @@
+﻿using OnlineStore.Extensions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace OnlineStore.PageObjects
+{
+    public class HomePage
+    {
+        [FindsBy(How = How.Id, Using = "account")][CacheLookup]
+        private IWebElement MyAccount { get; set; }
+
+        public void ClickOnMyAccount()
+        {
+            MyAccount.ClickOnIt("MyAccount");
+        }
+    }
+}
---
+++ OnlineStore/TestCases/LoginTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using OnlineStore.PageObjects;
+using OnlineStore.WrapperFactory;
+using System.Configuration;
+
+
+namespace OnlineStore.TestCases
+{
+    class LoginTest
+    {
+        [Test]
+        public void Test()
+        {
+            BrowserFactory.InitBrowser("Chrome");
+            BrowserFactory.LoadApplication(ConfigurationManager.AppSettings["URL"]);
+
+            Page.Home.ClickOnMyAccount();
+            Page.Login.LoginToApplication("LoginTest");
+
+            BrowserFactory.CloseAllDrivers();
+        }
+    }
+}
